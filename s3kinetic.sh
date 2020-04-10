689e760ec64c928ad50b5198b60662d3e4a49ba7
Thai Nguyen <thai.c.nguyen@seagate.com>
2020-04-10 16:43:10 -0600

    s3 kinetic in one place

--- s3kinetic.sh
+++ s3kinetic.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
+#Command:
+# s3kinetic ARM LAMARRKV
+# s3kinetic X86 SMR
+# s3kinetic X86 NONSMR
 git clean -xdf
 cd kineticd
 git clean -xdf
-if [ "$1" = "X86" ]
-then
-  source ./x86_envsetup.sh
-  cmake -DPRODUCT=X86
-  make
-  ./cplibx86.sh ../libx86
-  cd ../
-  make -f Makefile.x86
-else
-  echo "ARM"
-  source ~/uboot-linux/envsetup.sh
-  cmake -DPRODUCT="$1"
-  make
-  ./cplib.sh ../libarm
-  cd ../
-  make -f Makefile.arm
-fi
+case "$1" in 
+  "X86" )
+     source ./x86_envsetup.sh
+     case "$2" in
+       "NONSMR" )
+          cmake -DPRODUCT=X86NONSMR
+       ;;
+ 
+       "SMR" )
+          cmake -DPRODUCT=X86SMR
+       ;;
+     esac
+     make
+     ./cplibx86.sh ../libx86
+     cd ../
+     make -f Makefile.x86
+     ;;
 
+  "ARM" )
+    echo "ARM"
+    source ~/uboot-linux/envsetup.sh
+    cmake -DPRODUCT="$1"
+    make
+    ./cplib.sh ../libarm
+    cd ../
+    make -f Makefile.arm
+    ;;
+esac
