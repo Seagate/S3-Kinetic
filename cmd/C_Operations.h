c6bcb2cbd3d811b853e33d3a55c4be4247ef3dd5
Tri Nguyen <tri.c.nguyen@seagate.com>
2021-06-28 13:07:52 -0600

    Work

--- cmd/C_Operations.h
+++ cmd/C_Operations.h
@@ -4,8 +4,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
-//#include "kernel_mem_mgr.h"
-
 #ifndef __cplusplus
 #define bool _Bool
 #define true 1
@@ -18,38 +16,36 @@
 #endif
 
 
-#ifdef _cplusplus
+#ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef struct _CPrimaryStoreValue {
+    int metaSize;
+    int32_t algorithm;
     char* version;
     char* tag;
     char* meta;
-    int metaSize;
-    int32_t algorithm;
 } _CPrimaryStoreValue;
 
 
 char* allocate_pvalue_buffer(int size);
 void deallocate_gvalue_buffer(char* buff);
 
-void CInitMain(char* store_partition);
 
-//char* Get(int64_t user_id, char* key, struct _CPrimaryStoreValue* psvalue, char*** buff, uint32_t* size, uint32_t* status);
 char* Get(int64_t user_id, char* key, char* bvalue, struct _CPrimaryStoreValue* psvalue, int* size, int* status);
-char* GetMeta(int64_t user_id, char* key, struct _CPrimaryStoreValue* psvalue, int* size, int* st);
+char* GetMeta(int64_t user_id, char* key, struct _CPrimaryStoreValue* psvalue, int* size, int* status);
 
 int Put(int64_t user_id, char* key, char* current_version, _CPrimaryStoreValue* psvalue, char* value, size_t size,
-         _Bool sync, int64_t sequence, int64_t connID);
+          _Bool sync, uint64_t sequence, int64_t connID);
 
-int Delete(int64_t user_id, char* key, char* current_version,  _Bool sync, int64_t sequence, int64_t connID);
+int Delete(int64_t user_id, char* key, char* current_version,  _Bool sync, uint64_t sequence, int64_t connID);
 
 void GetKeyRange(int64_t user_id, char* startKey, char* endKey, bool startKeyInclusive, bool endKeyInclusive, uint32_t maxReturned,
                  bool reverse, char* results, int* size);
 
-#ifdef _cplusplus
+#ifdef __cplusplus
 }
 #endif
-#endif  // KINETIC_MINIO_SKINNY_WAIST_H_
+#endif  // C_OPERATIONS_H_
 
